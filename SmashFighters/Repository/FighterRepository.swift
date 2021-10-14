688719c52c377ad1e30c5bfee51e0a060ed02f63
Allan Melo <allan.melo@picpay.com>
2021-10-14 19:16:43 -0300

    Create functions to get and save Fighters on Repository

--- SmashFighters/Repository/FighterRepository.swift
+++ SmashFighters/Repository/FighterRepository.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Combine
+import CoreData
 
 class FighterRepository {
     struct Constants {
@@ -27,5 +28,33 @@ class FighterRepository {
         }
         
         return DecodableLoader<[Fighter]>(url: url).load()
+    }
+    
+    class func saveFighters(_ fighters: [Fighter]) {
+        let viewContext = CoreDataManager.shared.viewContext
+        
+        loadFightersFromPersistence().forEach { viewContext.delete($0) }
+        
+        fighters.forEach { fighter in
+            let fighterCoreDataModel = FighterCoreDataModel(context: viewContext)
+            fighterCoreDataModel.id = fighter.objectID
+            fighterCoreDataModel.name = fighter.name
+            fighterCoreDataModel.universe = fighter.universe
+            fighterCoreDataModel.price = fighter.price
+            fighterCoreDataModel.popular = fighter.popular
+            fighterCoreDataModel.rate = Int16(fighter.rate)
+            fighterCoreDataModel.downloads = fighter.downloads
+            fighterCoreDataModel.desc = fighter.description
+            fighterCoreDataModel.createdAt = fighter.createdAt
+            fighterCoreDataModel.imageURL = fighter.imageURL
+        }
+        
+        try? viewContext.save()
+    }
+    
+    class func loadFightersFromPersistence() -> [FighterCoreDataModel] {
+        let viewContext = CoreDataManager.shared.viewContext
+        let fetchRequest = NSFetchRequest<FighterCoreDataModel>(entityName: "FighterCoreDataModel")
+        return (try? viewContext.fetch(fetchRequest)) ?? []
     }
 }
